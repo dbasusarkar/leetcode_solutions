15c58c4d180ee9c48b99571271f64dcfbe438aa2
dbasusarkar <12450334+dbasusarkar@users.noreply.github.com>
2024-02-14 06:12:58 -0700

    two sum updated

--- Main/src/com/leetcode/python/easy/two_sum/two_sum/test_solution.py
+++ Main/src/com/leetcode/python/easy/two_sum/two_sum/test_solution.py
@@ -1,12 +1,16 @@
 from Solution import Solution
 
+
 sol = Solution()
+
 
 def test_one():
     assert set(sol.twoSum([2,7,11,15], 9)) == {0, 1}
 
+
 def test_two():
     assert set(sol.twoSum([3,2,4], 6)) == {1, 2}
+
 
 def test_three():
     assert set(sol.twoSum([3,3], 6)) == {0, 1}
